65b6c2e630c2069e0fbded513bab49ea760024e5
aadriellyy <limaadrielly214@gmail.com>
2023-03-09 10:59:07 -0300

    Projeto LearnCode.Web em C#

---
+++ LearnCode.Web.Models/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCode.Web.Models
+{
+    //Estamos estudando design pattern POCO
+    [Table("Usuarios")]
+    public class Usuario
+    {
+
+    }
+    
+}
